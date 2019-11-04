834af7fd308414c16dd4efbcf78b299e578d500c
Yucon <Frijke28@hotmail.com>
2019-11-04 08:11:09 +0000

    add some settings

--- http.py
+++ http.py
@@ -3,6 +3,8 @@ import asyncio
 # remainder of functions omitted
 
 # Entrypoint to making requests
+
+
 def assault(url, requests, concurrency):
     results = []
     asyncio.run(distribute_work(url, requests, concurrency, results))
